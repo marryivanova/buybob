356136841680f86f8dd52a19b57759787d99927a
okomarova <okomarova@saber.games>
2026-06-29 20:14:27 +0300

    #11 bug: add valid lid

--- app/admin/settings.py
+++ app/admin/settings.py
@@ -1,7 +1,6 @@
-import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = "postgres://postgres:postgres@localhost:5432/contest"
-REDIS_URL = "redis://localhost:6380/0"
+
+DATABASE_URL = ""
+REDIS_URL = ""
